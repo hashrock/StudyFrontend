e13789acc971c59088839028d3cecd5219938b38
181dev <yuiwai.dev@gmail.com>
2014-09-02 10:11:21 +0900

    refactoring

--- kata10_vue_canvas/index.js
+++ kata10_vue_canvas/index.js
@@ -1,3 +1,25 @@
+/**
+ * 背景用の市松模様を描画
+ * @param contextBackground
+ * @param imageSize
+ * @param dotSize
+ */
+function drawBackground(contextBackground, imageSize, dotSize){
+    for(var i = 0; i < imageSize; i++){
+        for(var j = 0; j < imageSize; j++){
+            contextBackground.fillStyle=("rgb(240,240,240)");
+            if(i % 2 === 0 && j % 2 === 0){
+                contextBackground.fillStyle=("rgb(255,255,255)");
+            }
+            if(i % 2 !== 0 && j % 2 !== 0){
+                contextBackground.fillStyle=("rgb(255,255,255)");
+            }
+            contextBackground.fillRect(i * dotSize, j * dotSize, dotSize, dotSize);
+        }
+    }
+}
+
+
 new Vue({
     el:"#main",
     methods:{
@@ -23,18 +45,8 @@ new Vue({
 
         var mainCanvasBackground = document.getElementById("main-canvas-background");
         var contextBackground = mainCanvasBackground.getContext('2d');
-        for(var i = 0; i < imageSize; i++){
-            for(var j = 0; j < imageSize; j++){
-                contextBackground.fillStyle=("rgb(240,240,240)");
-                if(i % 2 === 0 && j % 2 === 0){
-                    contextBackground.fillStyle=("rgb(255,255,255)");
-                }
-                if(i % 2 !== 0 && j % 2 !== 0){
-                    contextBackground.fillStyle=("rgb(255,255,255)");
-                }
-                contextBackground.fillRect(i * dotSize, j * dotSize, dotSize, dotSize);
-            }
-        }
+
+        drawBackground(contextBackground, imageSize, dotSize);
 
         var mainCanvas = document.getElementById("main-canvas");
         var self = this;
